c6b20212124a1372466bb6e0232bf4983b932415
seadawy <k9335551@gmail.com>
2025-10-22 07:40:17 +0300

    feat: Levels CRUD with filament

---
+++ app/Filament/Resources/LevelResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LevelResource\Pages;
+use App\Models\Level;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class LevelResource extends Resource
+{
+    protected static ?string $model = Level::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->label('Title')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ])->searchable();
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLevels::route('/'),
+            'create' => Pages\CreateLevel::route('/create'),
+            'edit' => Pages\EditLevel::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/LevelResource/Pages/CreateLevel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\LevelResource\Pages;
+
+use App\Filament\Resources\LevelResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateLevel extends CreateRecord
+{
+    protected static string $resource = LevelResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
